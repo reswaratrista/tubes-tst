9f4de688de487963b7be64578a139b62b3bfdbed
reswaratrista <18221122@std.stei.itb.ac.id>
2023-11-05 20:33:53 +0700

    Update main.py and similarities.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fastapi.responses import RedirectResponse
 
 from routes.users import user_router
 from routes.movies import movie_router
+from routes.movieCategories import moviecategory_router
+from routes.similarities import similarity_router
+from routes.categories import category_router
 from database.connection import conn
 
 app = FastAPI()
@@ -11,6 +14,9 @@ app = FastAPI()
 # Register routes
 app.include_router(user_router, prefix="/user")
 app.include_router(movie_router, prefix="/movie")
+app.include_router(moviecategory_router, prefix="/moviecategory")
+app.include_router(similarity_router, prefix="/similarity")
+app.include_router(category_router, prefix="/category")
 
 
 @app.on_event("startup")
--- routes/similarities.py
+++ routes/similarities.py
@@ -5,13 +5,13 @@ from fastapi import APIRouter, Depends, HTTPException, Request, status
 from models.similarities import Similarity
 from sqlmodel import select
 
-movie_router = APIRouter(
+similarity_router = APIRouter(
     tags=["Similarity"],
 )
 
 movie_database = Database(Similarity)
 
-@movie_router.get("/", response_model=List[Similarity])
+@similarity_router.get("/", response_model=List[Similarity])
 async def retrieve_all_events(session=Depends(get_session)) -> List[Similarity]:
     statement = select(Similarity)
     events = session.exec(statement).all()
